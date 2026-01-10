a4bc2f4d03ca843547f740fd1b741622cd778f14
Ilya <xorvar.longom@gmail.com>
2026-01-10 22:53:16 +0300

    forms validation

--- src/components/Sider/SectionForm/SectionForm.jsx
+++ src/components/Sider/SectionForm/SectionForm.jsx
@@ -6,12 +6,40 @@ import Two from '../../../assets/images/Forms/Two.png'
 import Three from '../../../assets/images/Forms/Three.png'
 import For from '../../../assets/images/Forms/For.png'
 import Five from '../../../assets/images/Forms/Five.png'
+import { useState } from 'react';
 
 export default function SectionForm() {
+  const [name, setName] = useState("")
+  const [tel, setTel] = useState("")
+  const [errors, setErrors] = useState({})
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const newErrors = {}
+
+    if (!name.trim()) {
+      newErrors.name = "Name necessarily"
+    }
+
+    if (!tel.trim()) {
+      newErrors.tel = "Without a phone number we won't be able to contact you."
+    } else if (!tel.includes("+")) {
+      newErrors.tel = `Incorrect phone add "+"`
+    }
+
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+      alert("Form submitted!")
+      location.reload()
+    }
+  }
+
     return (
         <section className={styles.forms}>
             <div className={clsx('container', styles.forms__div)}>
-                <form className={styles.forms__form}>
+                <form onSubmit={handleSubmit} className={styles.forms__form}>
                     <div className={styles.forms__form__block}>
                         <p className={styles.forms__form__block__h}>Any questions?</p>
                         <p className={styles.forms__form__block__p}>Fill out the form and our manager will contact you</p>
@@ -20,16 +48,30 @@ export default function SectionForm() {
                         <label className={styles.forms__form__div__label} htmlFor="name">
                             <input 
                                 className={styles.forms__form__div__inpt} 
-                                type="name" 
+                                type="text"
+                                value={name}
+                                style={{
+                                        border: errors.name ? '1px solid red' : null,
+                                    }}  
+                                onChange={(e) => setName(e.target.value)}
                                 id='name'
-                                placeholder='Your name' />
+                                placeholder='Your name'
+                                 />
+                                {errors.name && <p className={styles.forms__form__div__pError}>{errors.name}</p>}
                         </label>
                         <label className={styles.forms__form__div__label} htmlFor="tel">
                             <input 
                                 className={styles.forms__form__div__inpt} 
-                                type="phone" 
+                                type="text" 
                                 id='tel'
-                                placeholder='Phone number' />
+                                value={tel}
+                                style={{
+                                        border: errors.tel ? '1px solid red' : null,
+                                    }} 
+                                onChange={(e) => setTel(e.target.value)}
+                                placeholder='Phone number'
+                                 />
+                                 {errors.tel && <p className={styles.forms__form__div__pError}>{errors.tel}</p>}
                         </label>
                         <button type='submit' className={styles.forms__form__div__btn}>Submit</button>
                     </div>
